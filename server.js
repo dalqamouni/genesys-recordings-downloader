cc711fa8af2de9af72f97300f1e824e7774a0dc0
Genesys_Cloud <150661355+dalqamouni@users.noreply.github.com>
2025-03-22 21:52:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Genesys Recordings Downloader</title>
+
+
 // Import built in libraries needed.
 const http = require('https');
 const fs = require('fs');
