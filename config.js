3e67a70b6ecd141fb0e81ad86bb0f5e1be010b20
Genesys_Cloud <150661355+dalqamouni@users.noreply.github.com>
2025-03-23 08:28:37 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+// JavaScript source code
+(function (window) {
+  window.__config = window.__config || {};
+
+  window.__config.apiUrl = "https://192.168.50.19/ConversationsAnywhereAPI/";
+
+  window.__config.ftpPath = "https://192.168.50.19/ConversationsAnywhereFTP/";
+
+  window.__config.enableDebug = true;
+
+  window.__config.baseHref = "/ConversationsAnywhere/";
+
+  window.__config.sessionExpiration = 20;
+
+  window.__config.GenesysClientID = '8c8eedc6-6f81-4f7b-a86e-3328be2795bd';
+
+  window.__config.GenesysClientSecret = 'q1os2R76Y-xPBk8bGd2XrsoOQCqpWlSRy11aVUFULxU';
+
+  window.__config.GenesysHost = 'mypurecloud.ie';
+
+  window.__config.siteUrl = '';
+
+}(this));
